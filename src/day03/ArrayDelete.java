199458295fc778d6b0ba3dde54cfaa15e19752c5
JeFeel <jaephil94@gmail.com>
2023-04-03 15:40:10 +0900

    주석 추가

--- src/day03/ArrayDelete.java
+++ src/day03/ArrayDelete.java
@@ -13,6 +13,17 @@ public class ArrayDelete {
       arr[i] = arr[i+1];
     }
     //pop
+    /*
+    arr[1]=arr[2];
+    arr[2]=arr[3];
+    arr[3]=arr[4];
+    arr[4]=arr[5];
+    */
+
+
+
+
+
     int[]temp = new int[arr.length -1];
     for (int i = 0; i < temp.length; i++) {
       temp[i] = arr[i];
