71304a24ced5e1e3d3c79007bbfcbf287ebde67f
JeFeel <jaephil94@gmail.com>
2023-04-05 20:34:10 +0900

    오버로드 연습

---
+++ src/day08/poly/overloading/Main.java
@@ -0,0 +1,14 @@
+package day08.poly.overloading;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Over over = new Over(); //Over 클래스세어 넘어와서 객체 생성
+        over.test(); //Over 클래스에 안에 있는 test() 메서드 사용
+        //test메서드는 내부에 오버로드된 동명의 메서드들을 지님
+
+
+    }
+
+}
--- src/day08/poly/overloading/Over.java
+++ src/day08/poly/overloading/Over.java
@@ -2,12 +2,12 @@ package day08.poly.overloading;
 
 
 /*
-* 오바라이딩: 부모클래스의 메서드를 자식이 재정의
-* 오버로딩: 하나의 클래스 안에서 같은 이름의 메서드를 여러 개 선언
+* 오버라이딩: 부모클래스의 메서드를 자식이 재정의 Override
+* 오버로딩: 하나의 클래스 안에서 ☆같은 이름의 메서드를 여러 개 선언☆
 *           단, 반드시 파라미터 (매개변수의 타입, 순서, 개수)가 달라야 함
-*           리턴타입과 접근제한자는 관련이 없다.
+*           리턴타입과 접근제한자는 관련이 없다. Overload
 * */
-public class Over {
+public class Over{
 
 
     // alertType(String)
@@ -17,17 +17,21 @@ public class Over {
     }
     // alertType(int)
     void alertType(int x){ //타입이 다름
-        System.out.println("전달된 데이터는 정수입니다.");
+        System.out.printf("전달된 데이터는 정수입니다. 정수: %d\n", x);
     }
     // alertType(int, int)
-    void alertType(int x, int y){}//개수가 다름
+    void alertType(int x, int y){
+        System.out.printf("전달된 데이터는 정수 2개입니다. 정수1: %d, 정수2: %d\n", x,y);
+    }//개수가 다름
 
     void alertType(int x, String y){}
     void alertType(String x, int y){}
 
 
     void test(){
-        alertType("zzz", 10);
-        System.out.println();
+        alertType("zzz");
+        alertType(10);
+        alertType(7,15);
+//        System.out.println();
     }
 }
